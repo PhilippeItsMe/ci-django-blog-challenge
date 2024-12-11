e03de5b73c546f35e330ce7495183187bd1314dc
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-11 13:44:59 +0000

    Debuging turned to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','8000-philippeits-cidjangoblo-1sz3ki8vx2c.ws-eu117.gitpod.io','https://8000-philippeits-cidjangoblo-1sz3ki8vx2c.ws-eu117.gitpod.io', 'localhost']
 
