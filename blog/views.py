09e17092497640d094e895e636ad3fa66c188fa4
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-11 15:56:47 +0000

    Debugging importer data from json file

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
     # queryset = Post.objects.all().order_by("created_on")
-    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
    
