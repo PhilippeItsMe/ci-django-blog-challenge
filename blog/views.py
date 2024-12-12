d331c0e4ea3ba9578c7156f5ae2d1055aa1a48b2
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-12 14:01:24 +0000

    Debuging blog/views, taking out the old way of doing it

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,13 @@ from .models import Post
 
 # Create your views here.
 
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    # queryset = Post.objects.all().order_by("created_on")
-    # queryset = Post.objects.filter(status=1)
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-   
+
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -24,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
