39768c48ed996585db5d22b5130f48fbcc9a311c
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-19 10:20:59 +0000

    Ad delete button

--- about/views.py
+++ about/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
 
 def about_me(request):
+
     """
-    Renders the About page
+    Save the collaborate_form into the dB
+    """
+
+    if request.method == "POST":
+            collaborate_form = CollaborateForm(data=request.POST)
+            if collaborate_form.is_valid():
+                collaborate_form.save()
+                messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
+    """
+    Collect the key informations
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm
+
+    """
+    Render the about page
+    """
 
     return render(
         request,
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log('Hello, world! You look great today.')
